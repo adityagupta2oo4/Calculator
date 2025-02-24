770684f7cc45d15eb6b3ba74808f2f415cb625ec
Aditya Gupta <ag75100@gmail.com>
2025-02-24 07:36:41 +0000

    Prepared the front-end using Html and Css

---
+++ script.js
@@ -0,0 +1,54 @@
+/*  Pseudo Code or Logic
+
+
+
+*/ 
+
+//operation function
+function sum(){
+
+}
+
+function subtract() {
+    
+}
+
+function multiply(){
+
+}
+
+function divide(){
+
+}
+
+// one function to hande them all
+
+function operate(num1,op,num1){
+
+}
+
+
+//global variable to store the digit
+let num1;
+let num2;
+let op = "";
+
+//to display the digit
+
+function display(){
+    // strore the content too  in global variable onyly one number
+}
+
+//event handler when the button is press
+
+function senseClick(){
+
+    //make sure that is if we 9 then + then 7 then if user ennter 4 first evaluate the pair and then do next ooperation
+    // so trigger point for operation be either = or any other operator if we have a pair
+
+}
+
+
+
+
+
